5fc74252c1da7a481f5b497fa3085c05aff5169d
Rohit Sharma <141807308+sharmarohit45@users.noreply.github.com>
2024-08-05 01:51:34 +0530

    Update CorsConfiguration.java

--- src/main/java/com/youtube/jwt/configuration/CorsConfiguration.java
+++ src/main/java/com/youtube/jwt/configuration/CorsConfiguration.java
@@ -1,3 +1,4 @@
+
 package com.youtube.jwt.configuration;
 
 import org.springframework.context.annotation.Bean;
@@ -8,15 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration {
 
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String PUT = "PUT";
+    private static final String DELETE = "DELETE";
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://psspl.netlify.app",)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Explicitly allow OPTIONS
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://psspl.netlify.app")
+                        .allowedMethods(GET, POST, PUT, DELETE)
                         .allowedHeaders("*")
+                        .allowedOriginPatterns("*")
                         .allowCredentials(true);
             }
         };
