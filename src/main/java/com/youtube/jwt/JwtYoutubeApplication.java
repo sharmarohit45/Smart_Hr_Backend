3228f950f337a44ff3c879214ad92750f615797b
Rohit Sharma <141807308+sharmarohit45@users.noreply.github.com>
2024-08-05 13:55:07 +0530

    Update JwtYoutubeApplication.java

--- src/main/java/com/youtube/jwt/JwtYoutubeApplication.java
+++ src/main/java/com/youtube/jwt/JwtYoutubeApplication.java
@@ -3,11 +3,11 @@ package com.youtube.jwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.youtube.jwt.dao")
-
+@CrossOrigin(origins = "https://psspl.netlify.app/")
 public class JwtYoutubeApplication {
 
     public static void main(String[] args) {
