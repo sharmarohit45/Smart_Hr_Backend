504a05dd0ec7f30dc257b34e143d39b69e9c5419
Rohit Sharma <141807308+sharmarohit45@users.noreply.github.com>
2024-08-05 12:32:00 +0530

    Update CorsConfiguration.java

--- src/main/java/com/youtube/jwt/configuration/CorsConfiguration.java
+++ src/main/java/com/youtube/jwt/configuration/CorsConfiguration.java
@@ -1,29 +1,22 @@
-package com.youtube.jwt.configuration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
-
-    private static final String GET = "GET";
-    private static final String POST = "POST";
-    private static final String PUT = "PUT";
-    private static final String DELETE = "DELETE";
+public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000", "https://psspl.netlify.app") // Exact origin URLs
-                    .allowedMethods(GET, POST, PUT, DELETE)
-                    .allowedHeaders("*") // Allows all headers; consider specifying if you need stricter control
-                    .allowCredentials(true); // Allow credentials to be sent with requests
+                registry.addMapping("/")
+                        .allowedOrigins("https://psspl.netlify.app", "http://localhost:3000") // Add other local ports if necessary
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
-        };
-    }
+        };
+    }
 }
