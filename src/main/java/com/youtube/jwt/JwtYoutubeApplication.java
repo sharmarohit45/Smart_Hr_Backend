a388d92bd7dde681eaf420be1396372b26c95dd9
Rohit Sharma <141807308+sharmarohit45@users.noreply.github.com>
2024-08-05 08:03:04 +0530

    Update ccc

--- src/main/java/com/youtube/jwt/JwtYoutubeApplication.java
+++ src/main/java/com/youtube/jwt/JwtYoutubeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.youtube.jwt.dao")
+@CrossOrigin
 public class JwtYoutubeApplication {
 
     public static void main(String[] args) {
