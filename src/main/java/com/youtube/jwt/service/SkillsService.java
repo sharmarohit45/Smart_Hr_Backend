65ae8617b26174d6d0654b76cca8ac5ef70dcc64
Rohit Sharma <141807308+sharmarohit45@users.noreply.github.com>
2024-08-07 22:11:27 +0530

    Update SkillsService.java

--- src/main/java/com/youtube/jwt/service/SkillsService.java
+++ src/main/java/com/youtube/jwt/service/SkillsService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.youtube.jwt.dao.SkillsDao;
 import com.youtube.jwt.entity.Skills;
+
 @Service
 public class SkillsService {
 @Autowired
