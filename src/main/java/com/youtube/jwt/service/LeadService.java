488e0800c0a6eb57d9494e3d3df2caf62b6b60fb
sharmarohit45 <officialrishi45@gmail.com>
2024-08-08 17:36:46 +0530

    Lead update

--- src/main/java/com/youtube/jwt/service/LeadService.java
+++ src/main/java/com/youtube/jwt/service/LeadService.java
@@ -1,7 +1,9 @@
 package com.youtube.jwt.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.youtube.jwt.dao.LeadDao;
 import com.youtube.jwt.entity.Lead;
@@ -30,4 +32,26 @@ public class LeadService {
     	leadDao.deleteById(id);
     }
     
+    public Lead updateLeadContact(Long id, Lead updatedContact) {
+    	Lead existingContact = leadDao.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Lead contact not found"));
+
+        existingContact.setSalutation(updatedContact.getSalutation());
+        existingContact.setName(updatedContact.getName());
+        existingContact.setEmail(updatedContact.getEmail());
+        existingContact.setLeadSource(updatedContact.getLeadSource());
+        existingContact.setAddedBy(updatedContact.getAddedBy());
+        existingContact.setCompanyName(updatedContact.getCompanyName());
+        existingContact.setWebsite(updatedContact.getWebsite());
+        existingContact.setMobile(updatedContact.getMobile());
+        existingContact.setOfficePhone(updatedContact.getOfficePhone());
+        existingContact.setCountry(updatedContact.getCountry());
+        existingContact.setState(updatedContact.getState());
+        existingContact.setCity(updatedContact.getCity());
+        existingContact.setPostalCode(updatedContact.getPostalCode());
+        existingContact.setAddress(updatedContact.getAddress());
+        existingContact.setSavedAt(updatedContact.getSavedAt());
+
+        return leadDao.save(existingContact);
+    }
 }
